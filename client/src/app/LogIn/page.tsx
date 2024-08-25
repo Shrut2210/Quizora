84eb37bc72af63d15d0ab4afe53542c11c66ee74
Shruti Vadgama <125206895+Shrut2210@users.noreply.github.com>
2024-08-25 13:32:52 +0530

    scb

--- client/src/app/LogIn/page.tsx
+++ client/src/app/LogIn/page.tsx
@@ -7,26 +7,26 @@ export default function LogIn() {
     const [isLogin, setIsLogin] = useState(true);
     return (
         <>
-        <div className="w-screen mx-auto rounded-md  h-screen overflow-hidden">
+        <div className="w-screen mx-auto font-serif rounded-md  h-screen overflow-hidden">
             <Vortex
                 backgroundColor="black"
-                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-            >
-                <div className="text-white text-2xl md:text-6xl font-bold text-center">
+                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+                <div className="text-white text-2xl md:text-6xl font-bold text-center w-4/5 h-3/4">
                 {
                 isLogin ?
                     // login side
-                    <div className="flex h-full backdrop-blur-sm backdrop-filter bg-opacity-10">
+                    <div className="flex h-full backdrop-blur-sm backdrop-filter bg-opacity-10 bg-purple-400">
                         {/* left part / login form */}
-                        <div className="text-white m-5 ">
+                        <div className="text-white m-5 flex w-2/5 items-center">
                                     
-                            <form className="flex flex-col">
-                                <h1 className="text-center">Login</h1>
-                                <label htmlFor="" className="text-xl">Email</label>
-                                <input type="email" placeholder="xyz@gmail.com" className="outline-none hover:outline-none bg-transparent rounded-md" required/>
+                            <form className="flex flex-col w-full">
+                                <h1 className="text-center">Log In</h1>
+                                <label htmlFor="" className="text-lg mt-16 text-start">Email</label>
+                                <input type="email" placeholder="xyz@gmail.com" className="border text-xl py-1 px-2 rounded-lg outline-none hover:outline-none bg-transparent" required/>
                                     
-                                <label htmlFor="">Password</label>
-                                <input type="password" placeholder="password" className="outline-none hover:outline-none bg-transparent" required/>
+                                <label htmlFor="" className="text-lg mt-6 text-start">Password</label>
+                                <input type="password" placeholder="password" className="border text-xl py-1 px-2 rounded-lg outline-none hover:outline-none bg-transparent" required/>
+                                <button type="submit" className="bg-blue-700 text-lg mt-10 py-1 rounded-md">Login</button>
                             </form>
                         </div>
                         
