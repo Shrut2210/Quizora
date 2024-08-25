5943191ea17cdea410d9be71b31e886ef4bae9e8
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-08-25 14:52:38 +0530

    UI updated spline

--- client/src/app/LogIn/page.tsx
+++ client/src/app/LogIn/page.tsx
@@ -3,36 +3,50 @@ import {useState } from "react"
 import { Boxes } from "@/components/BoxesCore";
 import { cn } from "@/lib/utils";
 import { Vortex } from "@/components/Vortex";
+import logo from "../../../public/assets/logo.png"
+import Image from "next/image";
+import Spline from '@splinetool/react-spline/next';
+
 export default function LogIn() {
     const [isLogin, setIsLogin] = useState(true);
     return (
         <>
         <div className="w-screen mx-auto font-serif rounded-md  h-screen overflow-hidden">
             <Vortex
-                backgroundColor="black"
+                    backgroundColor="black"
+                    baseHue={950}
+                    // baseSpeed={1}
+                    // rangeSpeed={15}
+                    rangeY={400}
+                    particleCount={300}
                 className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
                 <div className="text-white text-2xl md:text-6xl font-bold text-center w-4/5 h-3/4">
                 {
                 isLogin ?
                     // login side
-                    <div className="flex h-full backdrop-blur-sm backdrop-filter bg-opacity-10 bg-purple-400">
+                    <div className="flex h-full backdrop-blur-sm backdrop-filter bg-opacity-20">
                         {/* left part / login form */}
-                        <div className="text-white m-5 flex w-2/5 items-center">
+                        <div className="text-white flex w-1/2 justify-center items-center">
                                     
-                            <form className="flex flex-col w-full">
-                                <h1 className="text-center">Log In</h1>
-                                <label htmlFor="" className="text-lg mt-16 text-start">Email</label>
-                                <input type="email" placeholder="xyz@gmail.com" className="border text-xl py-1 px-2 rounded-lg outline-none hover:outline-none bg-transparent" required/>
+                            <form className="flex flex-col justify-center p-5 w-4/5">
+                                <div className="flex justify-center items-center">
+                                    <Image src={logo} className="h-1/2 w-1/2 " alt="botho"/>
+                                </div>
+                                <label htmlFor="" className="text-xl text-start py-2">Email</label>
+                                <input type="email" placeholder="xyz@gmail.com" className="border text-xl py-3 px-3 rounded-lg outline-none hover:outline-none bg-transparent" required/>
                                     
-                                <label htmlFor="" className="text-lg mt-6 text-start">Password</label>
-                                <input type="password" placeholder="password" className="border text-xl py-1 px-2 rounded-lg outline-none hover:outline-none bg-transparent" required/>
-                                <button type="submit" className="bg-blue-700 text-lg mt-10 py-1 rounded-md">Login</button>
+                                <label htmlFor="" className="text-xl mt-6 text-start py-2">Password</label>
+                                <input type="password" placeholder="password" className="border text-xl py-3 px-3 rounded-lg outline-none hover:outline-none bg-transparent" required/>
+                                <button type="submit" className="bg-[#9207E6] text-lg mt-10 py-4 rounded-md">Login</button>
                             </form>
                         </div>
                         
                         {/* right part / image / daglo */}
                         <div>
-
+                            <Spline
+                                            scene="https://prod.spline.design/qJO6EDvqMObwesFE/scene.splinecode" 
+                                            
+      />
                         </div>
                     </div> :
                     // signup side
