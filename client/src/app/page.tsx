66cdd24fb896ba199458b9c8f2fa771a22e8eb80
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-08-26 17:19:47 +0530

    Hero started 2

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import logo from "../../public/assets/logo.png" 
 import Spline from '@splinetool/react-spline/next';
 import Link from "next/link";
+import bg_1 from "../../public/assets/bg-1.png";
+
 export default function Home() {
   return (
     <>
       {/* Navbar */}
-      <div className="fixed p-5 flex justify-between items-center  w-full"> 
+      <div className="sticky p-5 flex justify-between items-center  w-full "> 
           <div className="">
            <Image src={logo} className="w-40" alt="Botho" /> 
           </div>
@@ -26,13 +28,23 @@ export default function Home() {
 
       {/* Image section */}
 
+    <div className="h-screen w-screen absolute -z-10 top-0 " >
+        <Image src={bg_1} alt="botho" className="h-full w-full"/>
+        <div className="h-full w-full bg-[#100D28] bg-opacity-80 absolute top-0 "></div>
+    </div>
       
       
 
 
       {/* smalll details sectoion */}
 
-
+      <div className="h-screen flex justify-end items-end absolute top-0 w-full overflow-hidden">
+        <div className="flex flex-col justify-center items-center rounded-3xl bg-transparent h-1/2 bg-white w-full text-5xl font-bold gap-3 rotate-2">
+          <div className="-rotate-2">Quizzing is the future of learning.</div>
+          <div className="-rotate-2">We craft quizzes that inspire.</div>
+          <div className="-rotate-2">We create quizzes that engage and educate.</div>
+        </div>
+      </div>
 
 
 
